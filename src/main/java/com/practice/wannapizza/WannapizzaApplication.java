5ddef8459ddecb1236e9e863910b68d3d0df51be
PDFAtauchi <floresatauchi@gmail.com>
2023-10-23 21:16:38 -0500

    chore: init project

---
+++ src/main/java/com/practice/wannapizza/WannapizzaApplication.java
@@ -0,0 +1,13 @@
+package com.practice.wannapizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannapizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannapizzaApplication.class, args);
+	}
+
+}
