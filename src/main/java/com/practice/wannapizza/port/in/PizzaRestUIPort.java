93006a19c059f85f57b8aa7116b773e973a852ef
PDFAtauchi <floresatauchi@gmail.com>
2023-10-24 16:45:05 -0500

    chore: remove unused imports

--- src/main/java/com/practice/wannapizza/port/in/PizzaRestUIPort.java
+++ src/main/java/com/practice/wannapizza/port/in/PizzaRestUIPort.java
@@ -1,12 +1,10 @@
 package com.practice.wannapizza.port.in;
 
 import com.practice.wannapizza.application.domain.model.Pizza;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PizzaRestUIPort {
     @PostMapping
